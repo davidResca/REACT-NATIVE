90931435cb20f1704b5efd2f4057d94d2e84ea7b
davidResca <davidresca9@gmail.com>
2023-09-03 21:24:25 -0300

    Agrega correccion en listItem - useState no utilizado

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import { AntDesign } from '@expo/vector-icons';
-import React, { useState } from 'react'
+import React from 'react'
 
 const ListItem = ({ item, clearItem, updateItem }) => {
 
